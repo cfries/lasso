6e9a33427141f097d7591b4e86e7c4abb8583637
Christian Fries <email@christian-fries.de>
2022-07-10 17:22:33 +0200

    Added subArray.

--- src/main/java/net/finmath/lasso/Utilities.java
+++ src/main/java/net/finmath/lasso/Utilities.java
@@ -3,6 +3,7 @@
  */
 package net.finmath.lasso;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 
 /**
@@ -18,7 +19,7 @@ public class Utilities {
 	 * @return product The matrix product of A*B (if suitable)
 	 */
 	public static double[][] mult(final double[][] left, final double[][] right) {
-		return new Array2DRowRealMatrix(left).multiply(new Array2DRowRealMatrix(right)).getData();
+		return (new Array2DRowRealMatrix(left)).multiply(new Array2DRowRealMatrix(right)).getData();
 	}
 
 	/**
@@ -29,6 +30,10 @@ public class Utilities {
 	 * @return product The vector product of A*v (if suitable)
 	 */
 	public static double[] mult(final double[][] matrix, final double[] vector) {
-		return new Array2DRowRealMatrix(matrix).operate(vector);
+		return (new Array2DRowRealMatrix(matrix)).operate(vector);
+	}
+	
+	public static double[] subArray(final double[] array, int startIndexInclusive, int endIndexExclusive) {
+		return ArrayUtils.subarray(array, startIndexInclusive, endIndexExclusive);
 	}
 }
